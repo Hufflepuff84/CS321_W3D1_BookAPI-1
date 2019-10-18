c6846477666a4f517c9ffa9a1fb3c82154659b24
Osman Samad <osamad84@yahoo.com>
2019-10-17 22:42:21 -0500

    first change.

--- CS321_W3D1_BookAPI/Models/Book.cs
+++ CS321_W3D1_BookAPI/Models/Book.cs
@@ -1,10 +1,22 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 using System.ComponentModel.DataAnnotations;
 
-namespace CS321_W3D1_BookAPI.Models
+namespace BookApi.Models
 {
     public class Book
     {
         public int Id { get; set; }
-    }
+        [Required(ErrorMessage = "Id is required")]
+        public string Title { get; set; }
+        [Required]
+        [MaxLength(75, ErrorMessage = "Title cannot be more than 75 characters")]
+        public string Author { get; set; }
+        [Required]
+        public string Category { get; set; }
+        
+
+    } 
 }
